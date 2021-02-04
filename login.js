db312b0aa47015e0cfe5c657dd15418f364e07e3
SebastianK <seebbe_@hotmail.com>
2021-02-04 11:58:46 +0100

    Fixed login page design fully

---
+++ login.js
@@ -0,0 +1,27 @@
+$('body').append(`<div class= "Login-border"></div>`);
+$('body').append('<hr class= "designLineTop"></hr>');
+
+$('body').append(`<div class= "username">
+<label>Användarnamn(E-postadress)</label>
+<input type="text" placeholder="Användarnamn">
+</div>`);
+
+$('body').append(`<div class= "password">
+<label>Lösenord</label>
+<input type="text" placeholder="Lösenord">
+<a href= "Just trying" class= "forgot">glömt ditt lösenord?</a>
+</div>`);
+
+$('body').append('<button class="login">Logga in</button>');
+
+$('body').append(`<div class= "lineContainer">
+<h2 class= "lineEller"> eller </h2>
+<hr class= "line"></hr>
+</div>`);
+
+$('body').append('<button class= "register">Registrera dig</button>');
+
+$('body').append('<hr class= "designLineBottom"></hr>');
+
+$('body').append('<h1 class= "title">Filmpunkten</h1>');
+$('body').append('<img src= "images/Filmpunkten.jpg" class= "titlePicture"></img>');
