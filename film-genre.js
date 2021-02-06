d66d11e8f8ca99db75e9d647464d1e7bb27967f0
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2021-02-06 19:40:34 +0100

    Chane css
    Changed Genre button

--- film-genre.js
+++ film-genre.js
@@ -2,7 +2,7 @@ $('.default').click(function () {
   $('.selectBox').slideToggle(200).css('borderTop', 'none');
   $('.selectBox li').click(function () {
     $('.mehidden').val($(this).text());
-    $('.default').text($(this).text());
+  
     $('.selectBox').slideUp(200);
   });
 });
