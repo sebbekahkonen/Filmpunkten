8ca0f9537e6918f7303f36a1c84b15ec9c2b5baa
Niklas Kullberg <NiklasKullberg@mail.com>
2021-02-09 08:32:56 +0100

    Restructured base-design with fixed header, footer, menu, content-box, image-content. And styling. Suggesting one base-design and all single-page-loads go into base-container. Also suggesting only one css since it's always the same page and since content varying is limited to smaller content-box. Design is also responsive to window-size-change. Needs finishing touches and also needs approval from group.

--- booking1.js
+++ booking1.js
@@ -1,19 +1,14 @@
-$('body').append(`<div class="container"></div>`);
-
-$('body').append(`<h2 class="choiceheader">Välj datum för att se vilka filmer som visas</h2>`);
 
 
 
-$('body').append(`
-  <div class="dateChoice">
-    <select id="selectyear">
-      <option value="selectyear">År</option>
-    </select>
-    <select id="selectmonth">
-      <option value="selectmonth">Månad</option>
-    </select>
-    <select id="selectday">
-      <option value="selectday">Dag</option>
-    </select>
-  </div>
-`);
+let container = $('.container');
+
+container.append(`<h2 class="choiceheader">Välj datum för att boka biljetter</h2>`);
+
+container.append(`<input type="date" id="datum" name="trip-start"
+    value="2021-02-08"
+    min="2021-01-01" max="2022-12-31"></input>`
+);
+
+
+
