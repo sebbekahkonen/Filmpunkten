7c33e02ce6496860c82b4df9efc9eee613355e62
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2021-02-15 19:28:18 +0100

    added files for film singlepage and styled some more

---
+++ JS-files/filmButtons.js
@@ -0,0 +1,14 @@
+
+
+$('.ticket').click(function () {
+});
+$('.button').click(function () {
+  
+  $('.selectBox').slideToggle(200).css('borderTop', 'none');
+  $('.selectBox li').click(function () {
+    $('.mehidden').val($(this).text());
+  
+    $('.selectBox').slideUp(200);
+    
+  });
+});
