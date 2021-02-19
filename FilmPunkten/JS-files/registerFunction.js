14644321262ff4b596d781b22b3552c98cc549b7
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2021-02-19 18:23:02 +0100

    Improved register function (now saves information to database)

--- FilmPunkten/JS-files/registerFunction.js
+++ FilmPunkten/JS-files/registerFunction.js
@@ -7,6 +7,12 @@ let phoneNumber = document.getElementById('phoneNumber');
 
 let canBePressed = true;
 
+async function createNewAccount() {
+  $('.emptyFields').remove();
+  console.log('Creating a new account...');
+  await db.run('INSERT INTO RegisterTable (username, password, firstName, lastName, phoneNumber) VALUES (?, ?, ?, ?, ?)', [username.value, password.value, firstName.value, lastName.value, phoneNumber.value]);
+}
+
 $('.registerNow').click(function () {
   if (username.value.length == 0 || password.value.length == 0 || repeatPassword.value.length == 0 || firstName.value.length == 0 || lastName.value.length == 0 || phoneNumber.value.length == 0) {
     if (canBePressed) {
@@ -14,6 +20,6 @@ $('.registerNow').click(function () {
       $('<h4 class="emptyFields">Vänligen fyll i alla fält.<h4>').appendTo('.lineoverLabels');
     }
   } else {
-    $('.emptyFields').remove();
+    createNewAccount();
   }
 });
