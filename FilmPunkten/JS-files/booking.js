7bc68623f72f3fe8a63fe3963e0f42e2a520fd51
niklas <NiklasKullberg@mail.com>
2021-02-20 15:11:43 +0100

    Adapted all my code to new approach to SPA. Fixed so that all my functions work with new code. Connected booking.js to database. created some test-data in two tables in DB and started planning of seats booking.

--- FilmPunkten/JS-files/booking.js
+++ FilmPunkten/JS-files/booking.js
@@ -1,71 +1,11 @@
-
 /*
-    GRUNDKOD MED LITE DATA FÖR ATT BYGGA FILM-SCHEMA FÖR BOKA-SIDAN
+bokning_bokningar
+id, user_id, boking_datum_id, stol(0-99999),   typ
+    1        1                1                vuxen
+    1        1                51                barn
 */
 
-let movies = [
-    {
-        "title": "Up",
-        "date":
-            [
-                { "view": "2021-03-01" },
-                { "view": "2021-02-24" }
-            ],
-        "runtime": "107 min",
-        "salong": "lillan"
-    },
-    {
-        "title": "Harry Potter",
-        "date":
-            [
-                { "view": "2021-03-01" },
-                { "view": "2021-02-20" }
-            ],
-        "runtime": "134 min",
-        "salong": "storan"
-    },
-    {
-        "title": "Inception",
-        "date":
-            [
-                { "view": "2021-03-01" },
-                { "view": "2021-02-24" },
-                { "view": "2021-02-28" }
-            ],
-        "runtime": "107 min",
-        "salong": "storan"
-    },
-    {
-        "title": "Avengers",
-        "date":
-            [
-                { "view": "2021-03-01" },
-                { "view": "2021-02-20" },
-                { "view": "2021-02-25" }
-            ],
-        "runtime": "134 min",
-        "salong": "mellan"
-    }
-];
 
-
-
-
-
-/*FUNKTIONER FÖR ATT BYGGA SIDORNA*/
-
-
-/*MAIN-SIDAN INITIALISERAS MED HTML FRÅN STARTSIDA.HTML (DÄR LIGGER ALLT CONTENT SOM SKA IN I CONTAINER FÖR STARTSIDA)*/
-
-buildMainHtml();
-
-async function buildMainHtml() {
-    let html = `
-    ${await $.get('/html-files/Startsida.html')}
-  `;
-
-    $('.container').html(html);
-}
 
 /*NEDANSTÅENDE LÄSER AV CLICK PÅ ALLA <A>-TAGGAR OCH FÖRHINDRAR ATT DEN GÅR TILL HTML-ADRESSEN. DEN HÄMTAR
        ISTÄLLET HTML-ADRESSENS CONTENT OCH LÄGGER IN DET I CONTAINER, DÄR ALL CONTENT SKA LIGGA.*/
@@ -73,80 +13,66 @@ async function buildMainHtml() {
 
 
 $(function () {
-    /*FUNKTION LÄNKAR I HEADER*/
-    $('.navigation a').click(function (e) {
-        e.preventDefault();
-
-        $.get($(this).attr("href"), function (data) {
-            $(".container").html(data);
-        });
-    });
-
-    /*FUNKTION LÄNKAR I FOOTER*/
-    $('footer a').click(function (e) {
-        e.preventDefault();
-
-        $.get($(this).attr("href"), function (data) {
-            $(".container").html(data);
-        });
-    });
-
-    $('.container').on("change", "#selectDate", function () {
+    
+    $('main').on("change", "#selectDate", function () {
         dateChoice = $(this).val();
+        
+        $('main').append($('#dateToView'));
+        $('main').append($('#moviesToView'));
 
-
-        $('.container').append($('#dateToView'));
-        $('.container').append($('#moviesToView'));
-
-        let moviesToDisplay = '';
-        let index = 1;
-        for (movie of movies) {
-            for (movieShow of movie.date) {
-                if (movieShow.view === dateChoice) {
-                    moviesToDisplay += `<button id="selectMovie" value="${movie.title}"> ${movie.title} </button><br>`;
-
-                    /*moviesToDisplay += movie.title + '&nbsp;' + '&nbsp;' + '&nbsp;' + '&nbsp;' + '&nbsp;' + '&nbsp;' + '&nbsp;' + '(runtime: ' + movie.runtime + ')' + '<br>';*/
-                }
-            }
-        }
-
-        if (moviesToDisplay.length > 2) {
-            $('#dateToView').html('<br>Välj film för att fortsätta:');
-            $('#moviesToView').html(moviesToDisplay);
-            $('#moviesToView').append('<div></div>');
-        }
-        else {
-            $('#dateToView').empty();
-            $('#moviesToView').html('Inga filmer visas på valt datum');
-        }
+        showMovies(dateChoice);
     });
 
+    
     let clickTitle;
 
-    $('.container').on('click', 'button', function () {
+    $('main').on('click', 'button', function () {
         clickTitle = $(this).attr('value');
 
         if (clickTitle !== undefined) {
-            $('#moviesToView div').html('<a href="tickets.html" id="nextStageBooking">Nästa</a>');
+            $('#moviesToView div').html('<a href="/html-files/tickets.html" id="nextStageBooking">Nästa</a>');
         }
     });
 
     /*FUNKTION LÄNK 'NÄSTA' PÅ BOOKING-SIDAN*/
-    $('.container').on('click', '#nextStageBooking', function (e) {
+    $('main').on('click', '#nextStageBooking', function (e) {
         e.preventDefault();
 
         $.get($(this).attr("href"), function (data) {
-            $(".container").html(data);
+            $("main").html(data);
             $('.chosenMovie span').text(clickTitle);
         });
 
     });
-
-
-
-
-
 });
+
+async function showMovies(dateChoice) {
+
+    let moviesToDisplay = '';
+    let result = await db.run(/*sql*/`
+        SELECT bokning_filmer_test.*
+        FROM bokning_filmer_test
+        JOIN bokning_datum
+        ON bokning_datum.bokning_filmer_test_id=bokning_filmer_test.id
+        WHERE bokning_datum.datum='${dateChoice}'
+        ORDER BY bokning_filmer_test.title
+    `);
+
+    for (let row of result) {
+        moviesToDisplay += `<button id="selectMovie" value="${row.title}"> ${row.title} </button><br>`;
+    }
+
+    if (moviesToDisplay.length > 2) {
+        $('#dateToView').html('<br>Välj film för att fortsätta:');
+        $('#moviesToView').html(moviesToDisplay);
+        $('#moviesToView').append('<div></div>');
+    }
+    else {
+        $('#dateToView').empty();
+        $('#moviesToView').html('Inga filmer visas på valt datum');
+    }
+}
+
 
 /*
 let moviePlay = movies.map(movies => movies.date.filter(movie => movie.date === '2021-02-24') );
@@ -199,4 +125,40 @@ for (let i = 0; i < movies.length; i++) {
 }
 */
 
+/*
+    // FUNKTION LÄNKAR I HEADER
+    $('.navigation a').click(function (e) {
+        e.preventDefault();
+
+        $.get($(this).attr("href"), function (data) {
+            $(".container").html(data);
+        });
+    });
+
+    // FUNKTION LÄNKAR I FOOTER
+    $('footer a').click(function (e) {
+        e.preventDefault();
+
+        $.get($(this).attr("href"), function (data) {
+            $(".container").html(data);
+        });
+    });
+
+
+// FUNKTIONER FÖR ATT BYGGA SIDORNA
+
+
+// MAIN-SIDAN INITIALISERAS MED HTML FRÅN STARTSIDA.HTML (DÄR LIGGER ALLT CONTENT SOM SKA IN I CONTAINER FÖR STARTSIDA)
+
+buildMainHtml();
+
+async function buildMainHtml() {
+    let html = `
+    ${await $.get('/html-files/Startsida.html')}
+  `;
+
+    $('.container').html(html);
+}
+
+*/
 
