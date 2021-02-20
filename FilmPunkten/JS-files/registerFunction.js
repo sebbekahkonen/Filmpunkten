5b3eeb64cad397345a98c3aa4cafb1d55f3f9c44
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2021-02-20 19:16:19 +0100

    Improved register page. Now input fields have to contain a certain amount of text and it is not possible to create white spaces. Also, the repeated password has to match the password.

--- FilmPunkten/JS-files/registerFunction.js
+++ FilmPunkten/JS-files/registerFunction.js
@@ -7,6 +7,50 @@ let phoneNumber = document.getElementById('phoneNumber');
 
 let canBePressed = true;
 
+function checkUsernameLength(usrnme) {
+  if (usrnme.length < 10 || usrnme.length > 30) {
+    alert('Ditt användarnamn måste bestå av 10-30 tecken!');
+  }
+}
+
+function checkPasswordLength(psswrd) {
+  if (psswrd.length < 8 || psswrd.length > 25) {
+    alert('Ditt lösenord måste bestå av 8-25 tecken!');
+  }
+}
+
+function checkRepeatedPassword(rptdPsswrd) {
+  if (rptdPsswrd !== password.value) {
+    alert('Lösenorden matchar inte!');
+  }
+}
+
+function checkFirstName(frstNme) {
+  if (frstNme.length < 2 || frstNme.length > 20) {
+    alert('Ditt förnamn måste bestå av 2-20 tecken!');
+  }
+}
+
+function checkLastName(lstNme) {
+  if (lstNme.length < 2 || lstNme.length > 25) {
+    alert('Ditt efternamn måste bestå av 2-25 tecken!');
+  }
+}
+
+function checkPhoneNumber(phnenmbr) {
+  if (phnenmbr.length < 10 || phnenmbr.length > 12) {
+    alert('Ditt telefonnummer måste bestå av 10-12 tecken!');
+  }
+}
+
+//Prevents white spaces
+function checkSpace(event) {
+  if (event.which == 32) {
+    event.preventDefault();
+    return false;
+  }
+}
+
 async function createNewAccount() {
   $('.emptyFields').remove();
   console.log('Creating a new account...');
@@ -15,10 +59,11 @@ async function createNewAccount() {
 
 $('.registerNow').click(function () {
   if (username.value.length == 0 || password.value.length == 0 || repeatPassword.value.length == 0 || firstName.value.length == 0 || lastName.value.length == 0 || phoneNumber.value.length == 0) {
-    if (canBePressed) {
-      canBePressed = false;
-      $('<h4 class="emptyFields">Vänligen fyll i alla fält.<h4>').appendTo('.lineoverLabels');
-    }
+    $('.emptyFields').remove();
+    $('<h4 class="emptyFields">Vänligen fyll i alla fält.<h4>').appendTo('.lineoverLabels');
+  } else if (username.value.length < 5 || username.value.length > 12) {
+    $('.emptyFields').remove();
+    $('<h4 class="emptyFields">Det finns fält som har för få/många tecken.<h4>').appendTo('.lineoverLabels');
   } else {
     createNewAccount();
   }
