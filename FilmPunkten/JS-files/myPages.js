e2a739688324e3a28ff49c9a3b625af9ddb2d2c1
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-03-01 21:39:57 +0100

    Fixed function for ongoingBooking, works almost the way I want, few changes left

--- FilmPunkten/JS-files/myPages.js
+++ FilmPunkten/JS-files/myPages.js
@@ -3,11 +3,9 @@ async function privateInformation() {
   SELECT *
   FROM RegisterTable
   `)
-
   let user = sessionStorage.getItem("user");
-  // $('.privateInfoDiv').remove();
   $("div.addMyPages").empty();
-  $("div.addMyPages").append(/* html */`
+  $("div.addMyPages").append(/*html*/`
   <div class="privateInfoDiv">
     <div class="privateNameDiv">
       <label id="privateName">Namn:</label>
@@ -32,55 +30,100 @@ async function privateInformation() {
 
 function bookingInformation() {
   $("div.addMyPages").empty();
-  $(".addMyPages").append(/* html */`
+  $(".addMyPages").append(/*html*/`
   <div class= "bookingInformation">
     <p>HistorikIsWorking</p>
   </div>`);
 }
 
 async function ongoingBookingInformation() {
-  let movies = await db.run(/*sql*/`
+  let movieBooking = await db.run(/*sql*/`
   SELECT *
   FROM booking
   `);
+  let showTimes = await db.run(/*sql*/`
+  SELECT * 
+  FROM show_times
+  `)
   let thisuser = await db.run(/*sql*/`
   SELECT * 
   FROM RegisterTable
   `)
-  let saveSeats = [];
+  let userSeats = await db.run(/*sql*/`
+  SELECT * 
+  FROM booking_seat
+  `)
+  let AllMovies = await db.run(/*sql*/`
+  SELECT * 
+  FROM FilmTable
+  `)
+  let saveBookingId = [];
+  let saveBookingMovie = [];
+  let num = 0;
+  let num1 = 0;
+  let userId = sessionStorage.getItem("user");
 
   $("div.addMyPages").empty();
-  let userId = sessionStorage.getItem("user");
-  for (let i = 0; i < movies.length; i++) {
-    if (thisuser[userId].username === movies[i].LoginTable_username) {
-      console.log("saved");
-      saveSeats[i] = movies[i].seat;
-      $(".addMyPages").append(/* html */`
-    <div class= "bookingInformation">
-      <div class= "bookedMovie">
-        <label id= "bookedMovieTitle">Film: </label>
-        <input for= "bookedMovieTitle" value="TheTitle">
-        <label id= "bookedMovieTickets">Biljett: </label>
-        <input for= "bookedMovieTickets" value="Vuxen/barn/pensionär">
-        <button>Ändra</button>
-        <button>Avboka</button>
-      </div>
-    </div>`);
+  movieBooking.forEach(function (booking) {
+    if (booking.RegisterTable_username === thisuser[userId].username) {
+      saveBookingId[num] = booking.id;
+      saveBookingMovie[num] = booking.show_times_id;
+      num++;
     }
-  }
-  if (saveSeats.length === 0) {
-    $(".addMyPages").append(/* html */`
-      <div class= "noBookings">
-        <h3>Du har inga bokningar för tillfället.</h3>
-      </div>
-    `)
-  }
+  });
+  AllMovies.forEach(function (movie) {
+    if (movie.id === saveBookingMovie[num1]) {
+      $(".addMyPages").append(/*html*/`
+        <div class= "bookingInformation">
+          <div class= "bookedMovie">
+            <label id= "bookedMovieTitle">Film: </label>
+            <input for= "bookedMovieTitle" id= "movieTitle" value=${movie.title}>
+            <label id= "bookedMovieGenre">Genre: </label>
+            <input for= "bookedMovieGenre" value="${movie.Genre}">
+            <button onClick= "location.href = '#html-movies/${movie.title}Page'">Filminfo</button>
+            <button onClick= "getMovieInfo()">Avboka</button>
+          </div>
+        </div>`);
+      num1++;
+      //    <label id= "bookedMovieSeat">Plats: </label>
+      //    <input for= "bookedMovieSeat" value= ""></input>
+    }
+  })
+
+  // let num2 = 0;
+  // userSeats.forEach(function (userBookingId) {
+  //   for (let i = 0; i < saveBookingId.length; i++) {
+  //     if (userBookingId.booking_id === saveBookingId[i]) {
+  //       if (userBookingId.booking_id === userBookingId.booking_id) {
+  //         console.log("it works");
+  //       }
+  //       $(".addMyPages").append(/*html*/`
+  //       <div class= "bookingInformation">
+  //         <div class= "bookedMovie">
+  //           <label id= "bookedMovieTitle">Film: </label>
+  //           <input for= "bookedMovieTitle" value="${saveBookingMovie[num2]}">
+  //           <label id= "bookedMovieSeat">Plats: </label>
+  //           <input for= "bookedMovieSeat" value= "${userBookingId.seat}">
+  //           <label id= "bookedMovieTickets">Biljett: </label>
+  //           <input for= "bookedMovieTickets" value="Vuxen/barn/pensionär">
+  //           <button>Ändra</button>
+  //           <button>Avboka</button>
+  //         </div>
+  //       </div>`);
+  //     }
+  //   }
+  // })
+}
+
+function getMovieInfo() {
+  let movieInfo = document.getElementById('movieTitle').value;
+  console.log(movieInfo);
 }
 
 
 function receiptsInformation() {
   $("div.addMyPages").empty();
-  $(".addMyPages").append(/* html */`
+  $(".addMyPages").append(/*html*/`
   <div class= "bookingInformation">
     <p>Kvitton is working</p>
   </div>`);
@@ -96,30 +139,18 @@ async function updateInformation(user) {
   let numberUpdate = document.getElementById('numberInput').value;
   if (nameUpdate != value[user].firstName) {
     await db.run('BEGIN TRANSACTION');
-    let thisval = await db.run('UPDATE RegisterTable SET firstName = (?) WHERE username = (?)', [nameUpdate, value[user].username]);
+    await db.run('UPDATE RegisterTable SET firstName = (?) WHERE username = (?)', [nameUpdate, value[user].username]);
     await db.run('COMMIT');
-
-    /* DESSA SKA BORT, BARA CHECK FOR NOW */
-    console.log("updatedName");
-    console.log(thisval);
   }
   if (lastnameUpdate != value[user].lastName) {
     await db.run('BEGIN TRANSACTION');
-    let thisval = await db.run('UPDATE RegisterTable SET lastName = (?) WHERE username = (?)', [lastnameUpdate, value[user].username]);
+    await db.run('UPDATE RegisterTable SET lastName = (?) WHERE username = (?)', [lastnameUpdate, value[user].username]);
     await db.run('COMMIT');
-
-    /* DESSA SKA BORT, BARA CHECK FOR NOW */
-    console.log(thisval);
-    console.log("updatedLastname");
   }
   if (numberUpdate != value[user].phoneNumber) {
     await db.run('BEGIN TRANSACTION');
-    let thisval = await db.run('UPDATE RegisterTable SET phoneNumber = (?) WHERE username = (?)', [numberUpdate, value[user].username]);
+    await db.run('UPDATE RegisterTable SET phoneNumber = (?) WHERE username = (?)', [numberUpdate, value[user].username]);
     await db.run('COMMIT');
-
-    /* DESSA SKA BORT, BARA CHECK FOR NOW */
-    console.log(thisval);
-    console.log("updatedNumber");
   }
 }
 
