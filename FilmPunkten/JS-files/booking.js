4b12c1750c48ae621fc2c0c5fc08173e9f5f3947
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-03-03 09:09:05 +0100

    Fixed function that if you are not logged in you cant make bookings.

--- FilmPunkten/JS-files/booking.js
+++ FilmPunkten/JS-files/booking.js
@@ -1,110 +1,120 @@
+if (document.getElementById('login_href').text === 'Min sida' === true) {
+    $('main').append(/*html*/`
+    <h2 class="choiceheader">Välj datum för att boka biljetter</h2>
+    <input type="date" id="selectDate" min="2021-01-01" max="2022-12-31">
+    <div id="dateToView"></div>
+    <p id="moviesToView"></p>
+    `)
+    $(function () {
 
+        //Sätt datumväljaren till dagens datum och max ett år framåt
+        let today = new Date();
+        let yyyy = today.getFullYear();
+        let mm = today.getMonth() + 1;
+        let dd = today.getDate();
 
+        if (mm < 10) { mm = '0' + mm; }
+        if (dd < 10) { dd = '0' + dd; }
 
+        let todaysDate = yyyy + '-' + mm + '-' + dd;
+        let maxDate = (yyyy + 1) + '-' + mm + '-' + dd;
 
-
-$(function () {
-    
-    //Sätt datumväljaren till dagens datum och max ett år framåt
-    let today = new Date();
-    let yyyy = today.getFullYear();
-    let mm = today.getMonth() + 1;
-    let dd = today.getDate();
-
-    if (mm < 10) { mm = '0' + mm; }
-    if (dd < 10) { dd = '0' + dd; }
-
-    let todaysDate = yyyy + '-' + mm + '-' + dd;
-    let maxDate = (yyyy + 1) + '-' + mm + '-' + dd;
-
-    $('#selectDate').attr({
-        'min': todaysDate,
-        'max': maxDate
-    });
-
-    //Samlar upp valt datum och visar filmer därefter
-    $('main').on("change", "#selectDate", function () {
-        dateChoice = $(this).val();
-        
-        $('main').append($('#dateToView'));
-        $('main').append($('#moviesToView'));
-
-        showMovies(dateChoice);
-    });
-
-    //Variabler för vald film, visningens id, biljetter, boka platser
-    let clickTitle;
-    selectedBookingId = 0;
-    tickets = [];
-    bookingSeats = '';
-
-    //Samlar upp vald film (titel), den valda filmens visnings-id
-    $('main').on('click', 'button', function () {
-        selectedBookingId = $(this).attr('value');
-        clickTitle = $(this).text();
-
-        //Om clickTitle har fått ett värde lägger vi till knapp "nästa"
-        if (clickTitle !== undefined) {
-            $('#moviesToView div').html('<a href="/html-files/tickets.html" id="nextStageBooking">Nästa</a>');
-        }
-    });
-
-    //Funktion för "nästa"-knappen på bokningssidan
-    $('main').on('click', '#nextStageBooking', function (e) {
-        e.preventDefault();
-
-        //läser in innehållet från tickets.html och tar även med vald titel
-        $.get($(this).attr("href"), function (data) {
-            $("main").html(data);
-            $('.chosenMovie span').text(clickTitle);
+        $('#selectDate').attr({
+            'min': todaysDate,
+            'max': maxDate
         });
-    });
 
+        //Samlar upp valt datum och visar filmer därefter
+        $('main').on("change", "#selectDate", function () {
+            dateChoice = $(this).val();
 
-    $('main').on('change', '#chooseTickets input', function (e) {
-        //"Boolean-värde för "nästa"-knapp i detta steg.
-        let showNext = false;
-        let count = 0;
+            $('main').append($('#dateToView'));
+            $('main').append($('#moviesToView'));
 
-        $('#chooseTickets input').each(function () {
-            //om någon biljett-typ har värde större än 0 visas knapp "nästa"
-            if ($(this).val() > 0) {
-                showNext = true;
+            showMovies(dateChoice);
+        });
+
+        //Variabler för vald film, visningens id, biljetter, boka platser
+        let clickTitle;
+        selectedBookingId = 0;
+        tickets = [];
+        bookingSeats = '';
+
+        //Samlar upp vald film (titel), den valda filmens visnings-id
+        $('main').on('click', 'button', function () {
+            selectedBookingId = $(this).attr('value');
+            clickTitle = $(this).text();
+
+            //Om clickTitle har fått ett värde lägger vi till knapp "nästa"
+            if (clickTitle !== undefined) {
+                $('#moviesToView div').html('<a href="/html-files/tickets.html" id="nextStageBooking">Nästa</a>');
             }
-            //fyller tickets med vald biljett-typ och antal med biljett-typ som nyckel till antal
-            tickets[$(this).attr('id')] = parseInt($(this).val());
-            count += parseInt($(this).val());
         });
 
-        tickets["total"] = count;
+        //Funktion för "nästa"-knappen på bokningssidan
+        $('main').on('click', '#nextStageBooking', function (e) {
+            e.preventDefault();
 
-        //visa "nästa"-knapp om boolean = true
-        if (showNext) {
-            $('#nextButtonTickets').show();
-        } else {
-            $('#nextButtonTickets').hide();
-        }
-    });
-
-    //Om klicka på nästa, ladda nästa, stolsbokning
-    $('main').on('click', '#nextButtonTickets', function (e) {
-        e.preventDefault();
-
-        $.get($(this).attr("href"), function (data) {
-
-            $("main").html(data);
-            bookingSeats = $('#bookingSeats');
-
-            //Kalla funktion för att skapa rendering av platser
-            createSeats();
+            //läser in innehållet från tickets.html och tar även med vald titel
+            $.get($(this).attr("href"), function (data) {
+                $("main").html(data);
+                $('.chosenMovie span').text(clickTitle);
+            });
         });
+
+
+        $('main').on('change', '#chooseTickets input', function (e) {
+            //"Boolean-värde för "nästa"-knapp i detta steg.
+            let showNext = false;
+            let count = 0;
+
+            $('#chooseTickets input').each(function () {
+                //om någon biljett-typ har värde större än 0 visas knapp "nästa"
+                if ($(this).val() > 0) {
+                    showNext = true;
+                }
+                //fyller tickets med vald biljett-typ och antal med biljett-typ som nyckel till antal
+                tickets[$(this).attr('id')] = parseInt($(this).val());
+                count += parseInt($(this).val());
+            });
+
+            tickets["total"] = count;
+
+            //visa "nästa"-knapp om boolean = true
+            if (showNext) {
+                $('#nextButtonTickets').show();
+            } else {
+                $('#nextButtonTickets').hide();
+            }
+        });
+
+        //Om klicka på nästa, ladda nästa, stolsbokning
+        $('main').on('click', '#nextButtonTickets', function (e) {
+            e.preventDefault();
+
+            $.get($(this).attr("href"), function (data) {
+
+                $("main").html(data);
+                bookingSeats = $('#bookingSeats');
+
+                //Kalla funktion för att skapa rendering av platser
+                createSeats();
+            });
+        });
+
     });
-    
-});
+} else {
+    $('main').append(/*html*/`
+    <h3>Du måste logga in för att kunna boka</h3>
+    <button onClick= "window.location.hash= '#login'" class= "bokaLogin">Logga in</button>
+    <p>eller</p>
+    <button onClick= "window.location.hash= '#register'" class= "bokaRegister">Registrera dig</button>
+    `);
+}
 
 chosenSeats = [];
 
-function createSeats()  {
+function createSeats() {
     let numOfSeats = 100;
     let html = "";
 
@@ -116,7 +126,7 @@ function createSeats()  {
             html += "</tr><tr>";
         }
         //Renderas med stolsnummer (och färg ledig/upptagen)
-        html += '<td data-seat="'+(i+1)+'">'+(i+1)+'</td>';
+        html += '<td data-seat="' + (i + 1) + '">' + (i + 1) + '</td>';
 
     }
     html += "</tr>";
@@ -143,19 +153,19 @@ function createSeats()  {
         } else {
             // Max seats selected
         }
-        
+
         if (chosenSeats.length == tickets['total']) {
             $('#nextButtonConfBooking').show();
         } else {
             $('#nextButtonConfBooking').hide();
         }
 
-        $('#bookingSeatsText span').text(tickets['total']-chosenSeats.length);
+        $('#bookingSeatsText span').text(tickets['total'] - chosenSeats.length);
     });
 
     $('#nextButtonConfBooking').click(function (e) {
         e.preventDefault();
-        
+
         let bookingNumber = Math.random().toString(36).substr(2, 8);
         let username = "user_" + bookingNumber;
         let chosenSeatsString1 = '<p>Du har valt plats: ';
@@ -209,7 +219,7 @@ async function insertSeat(bookingId, seatNumber) {
 }
 
 async function insertBooking(showTimesId, bookingNumber, username) {
-    
+
     await db.run('BEGIN TRANSACTION');
     await db.run(`
         INSERT INTO booking (
@@ -235,7 +245,7 @@ async function insertBooking(showTimesId, bookingNumber, username) {
 
 
 //Hämta från DB vilka platser som är upptagna och rendera dem annorlunda
-async function updateSeats()    {
+async function updateSeats() {
 
     let seats = [];
 
