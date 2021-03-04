5d60439bf14fec8725fc43fb5e749bb3ba5561e4
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2021-03-05 00:06:01 +0100

    Made a JS-file for film-genre

--- FilmPunkten/JS-files/film-genre.js
+++ FilmPunkten/JS-files/film-genre.js
@@ -1,8 +1,5 @@
-<main>
-
-  <h2 class='filmheader'>Aktuellt på bio</h2>
-  <link rel="stylesheet" href='/styles/film-genre.css'>
-<!--- <div id='buttonPosition'>
+$("main").append(/*html*/`
+  <div id='buttonPosition'>
     <span class="button">Genre &ensp; &nbsp; &or;</span>
     <input type="hidden" value="" class="mehidden" />
     <ul class="selectBox">
@@ -133,10 +130,4 @@
       </span>
     </a>
   </div>
--->
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-
-  <script src="/JS-files/filmButtons.js"></script>
-  <script src="/JS-files/film-genre.js"></script>
-</main>
+`)
